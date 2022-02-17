c9f9835e1a95eed70b247f99319190b68782ec95
Haakon <haakon.birk@gmail.com>
2022-02-17 14:37:57 +0100

    Added new recipe page with all fields

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import Recipes from "./components/dummy/recipes";
 import ProfilePage from "./components/dummy/profilePage";
 import NewRecipe from "./components/newRecipe/newRecipe";
 import Navbar from "./components/navbar/navbar";
-import AddIngredients from "./components/newRecipe/addIngredients";
 import RecipeFeed from "./components/recipeFeed/recipeFeed";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,7 +19,6 @@ render(<BrowserRouter>
         <Route path="/*" element={<App/>}/>
         <Route path="profilePage" element={<ProfilePage/>}/>
         <Route path="newRecipe" element={<NewRecipe/>}/>
-        <Route path="ingredients" element={<AddIngredients/>}/>
         <Route path="oppskrifter" element={<RecipeFeed/>}/>
         <Route path={"*"} element={<h1> 404: not found</h1>}/>
     </Routes>
