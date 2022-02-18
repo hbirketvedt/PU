70c66bf4e8f50410549b86599ab74c3863b38373
unknown <simeonc@stud.ntnu.no>
2022-02-18 15:51:50 +0100

    feat(#3): koblet profilside mot aktuell bruker

--- src/components/profile/profilePage.js
+++ src/components/profile/profilePage.js
@@ -6,39 +6,87 @@ import {
 import Navbar from "../navbar/navbar";
 import {getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
 import {Card, ListGroup, ListGroupItem} from "react-bootstrap";
+import { auth } from "../../firebase_config";
+import { onAuthStateChanged, signOut} from "firebase/auth";
+
 
 
 function ProfilePage() {
-    const documentID = "ajnqrGlobUEoQcuofgfR"
-    const imageurl = "profilepic.jpg"
     const usersCollectionRef = collection(db, "users")
-    const [username, setUsername] = useState()
-    const [phonenumber, setPhonenumber] = useState()
-    const [firstname, setFirstname] = useState()
-    const [surname, setSurname] = useState()
-    const [age, setAge] = useState()
-    const [bio, setBio] = useState()
-    const [url, setUrl] = useState("")
-    const starsRef = ref(getStorage(), `profilePictures/${imageurl}`);
+    const [email, setEmail] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [bio, setBio] = useState("")
+    const [imageURL, setImageURL] = useState("")
 
+    const [currentUser, setCurrentUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setCurrentUser(currentUser);
+        loadUser();
+    })
+
+    const loadUser = async () => {
+        const data = await getDocs(usersCollectionRef);
+        const user = data.docs.filter(doc => doc.id === currentUser.uid).reduce((a, b) => a).data();
+        setEmail(user.email);
+        setFirstname(user.firstName);
+        setLastName(user.lastName);
+        if (user.bio === "") {
+            setBio("Ingen bio");
+        } else {
+            setBio(user.bio);
+        }
+        handleDownload();
+    }
+        const handleDownload = async () => {
+            const imageRef = ref(getStorage(), 'profilePictures/' + currentUser.uid + '.png');
+            getDownloadURL(imageRef)
+                .then((url) => {
+                    setImageURL(url)
+                })
+                .catch((error) => {
+                    console.log(error.message)
+                    switch (error.code) {
+                        case 'storage/object-not-found':
+                            // File doesn't exist
+                            break;
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            break;
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            break;
+                        case 'storage/unknown':
+                            // Unknown error occurred, inspect the server response
+                            break;
+                     }
+                });
+        }
+
+
+    /*
     useEffect(() => {
         const loadUser = async () => {
             const data = await getDocs(usersCollectionRef);
-            const user = data.docs.filter(doc => doc.id === documentID).reduce((a, b) => a).data()
-            setUsername(user.username)
-            setPhonenumber(user.telephone)
-            setFirstname(user.firstname)
-            setSurname(user.surname)
-            setAge(user.age)
+            const user = data.docs.filter(doc => doc.id === currentUser.uid).reduce((a, b) => a).data()
+            //setUsername(user.username)
+            //setPhonenumber(user.telephone)
+            setEmail(user.email);
+            setFirstname(user.firstName)
+            console.log(user.firstName)
+            console.log("Heisa")
+            setSurname(user.lastName)
+            //setAge(user.age)
             setBio(user.bio)
-            console.log(user.username)
+            setImageURL(user.profilePictureURL)
+            console.log(user.email)
             getDownloadURL(starsRef)
                     .then((url) => {
-                        setUrl(url)
+                        setImageURL(url)
                     })
                     .catch((error) => {
-                        // A full list of error codes is available at
-                        // https://firebase.google.com/docs/storage/web/handle-errors
+                        console.log("Miss!")
                         switch (error.code) {
                             case 'storage/object-not-found':
                                 // File doesn't exist
@@ -60,31 +108,23 @@ function ProfilePage() {
         };
         loadUser();
         console.log("Database Polled");
-    }, []);
+    }, []); */
     
     return (
         <div>
             <Card style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={url} height="300px" width="300px" />
+                <Card.Img variant="top" src={imageURL} className="profileImage" />
                 <Card.Body>
-                    <Card.Text>
-                        {bio}
-                    </Card.Text>
+                    <Card.Text> {bio} </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    <ListGroupItem>Brukernavn: {username}</ListGroupItem>
-                    <ListGroupItem>Telefonnummer: {phonenumber}</ListGroupItem>
+                    <ListGroupItem>E-post: {email}</ListGroupItem>
                     <ListGroupItem>Fornavn: {firstname}</ListGroupItem>
-                    <ListGroupItem>Etternavn: {surname}</ListGroupItem>
-                    <ListGroupItem>Alder: {age}</ListGroupItem>
+                    <ListGroupItem>Etternavn: {lastName}</ListGroupItem>
+                    <p></p> 
                 </ListGroup>
             </Card>
-           {/*<img
-                src={url}
-                alt={""}
-                className={"profilePic"}
-          />*/}
-           </div>
+        </div>
     )
 }
 
