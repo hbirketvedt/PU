024ba6d1387628fa3cd4a85a0c1884edf194c819
kristff <kristff@stud.ntnu.no>
2022-03-01 14:16:03 +0100

    Made some small changes to the front page

--- src/components/dummy/splashPage.js
+++ src/components/dummy/splashPage.js
@@ -1,12 +1,17 @@
+import image1 from '../../images/forside-3.jpg'
+import image2 from '../../images/forside-2.jpg'
+import '../../css/app.scss'
+
 
 function SplashPage() {
     return(
-        <div>
+        <div id="splash">
             <h1>
-                Velkommen til vår React app
+                Velkommen til vår oppskriftsside
             </h1>
             <h3>
-                Dette er en dummy forside som ikke er fylt med noe spesifikt innhold foreløpig.
+                <img className="dummyImage" src={image1}></img>
+                <img className="dummyImage" src={image2}></img>
             </h3>
         </div>
     )
