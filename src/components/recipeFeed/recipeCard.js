773bf0bbf2a6bad0671b3b6e173f0b738369382b
unknown <simeonc@stud.ntnu.no>
2022-03-28 12:51:10 +0200

    endret bildestørrelse

--- src/components/recipeFeed/recipeCard.js
+++ src/components/recipeFeed/recipeCard.js
@@ -275,7 +275,7 @@ function RecipeCard(props) {
     return (
         <Card className={"card recipeCard"} style={{maxWidth: '100%', maxHeight: "34rem"}}>
             <Card.Img
-                style={{maxWidth: "30em", maxHeight: "20rem", objectFit: "cover"}}
+                style={{width: "30em", height: "20rem", objectFit: "cover"}}
                 variant="top" src={url}
             />
             <div className="centerIcon">
